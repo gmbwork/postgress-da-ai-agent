7fda0fb37f2c8286857f757cf1c2991c0efa9598
gmb <gmbwork0@gmail.com>
2023-12-11 16:55:19 +0530

    V2 - Autogen with OpentAI GPT-2.5 Turbo integration

--- postgres_da_ai_agent/main.py
+++ postgres_da_ai_agent/main.py
@@ -1,86 +1,183 @@
-
-import argparse
 import os
 from postgres_da_ai_agent.modules.db import PostgresManager
-from postgres_da_ai_agent.modules import llm
+# from postgres_da_ai_agent.modules import llm
 import dotenv
+import argparse
+import autogen
 
 dotenv.load_dotenv()
 
-assert os.environ.get("POSTGRES_URL") is not None, "POSTGRES_CONNECTION_URL not found in .env file"
+assert os.environ.get("DATABASE_URL") is not None, "POSTGRES_CONNECTION_URL not found in .env file"
 assert os.environ.get("OPENAI_API_KEY") != "", "OPENAI_API_KEY is empty in .env file"
 
-DB_URL = os.environ.get("POSTGRES_URL")
-OPEBNAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+DB_URL = os.environ.get("DATABASE_URL")
+#OPEBNAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
 POSTGRES_TABLE_DEFINITIONS_CAP_REF = "TABLE_DEFINITIONS"
 RESPONSE_FORMAT_CAP_REF = "RESPONSE_FORMAT"
-
 SQL_DELIMITER = "----------"
 
+def add_cap_ref(
+    prompt: str, prompt_suffix: str, cap_ref: str, cap_ref_content: str
+) -> str:
+    """
+    Attaches a capitalized reference to the prompt.
+    Example
+        prompt = 'Refactor this code.'
+        prompt_suffix = 'Make it more readable using this EXAMPLE.'
+        cap_ref = 'EXAMPLE'
+        cap_ref_content = 'def foo():\n    return True'
+        returns 'Refactor this code. Make it more readable using this EXAMPLE.\n\nEXAMPLE\n\ndef foo():\n    return True'
+    """
+
+    new_prompt = f"""{prompt} {prompt_suffix}\n\n{cap_ref}\n\n{cap_ref_content}"""
+
+    return new_prompt
+
+
 def main():
-    
     # parse prompt param using arg parse
     parser = argparse.ArgumentParser()
     parser.add_argument('--prompt', help='The prompt to use for the AI agent')
     args = parser.parse_args()
 
-    prompt = args.prompt
+    if not args.prompt:
+        print("Please provide a prompt using the --prompt flag")
+        return
+    
+    prompt = f"Fulfill this databse query: {args.prompt}. "
 
-    # connect to db using with statement and create a db_manager
     with PostgresManager() as db:
-
-        print("prompt v1", prompt)
-        #"postgres://postgres:postgres@localhost:5432/postgres"
         db.connect_with_url(DB_URL)
 
-    # call db_manager.get_table_definition() to get tables in prompt ready form
-    table_definitions = db.get_table_definition_for_prompt()
-    print("table_definitions", table_definitions)
+        table_definitions = db.get_table_definitions_for_prompt()
 
-    # create two blank calls to llm.add_cap_ref() that update our current prompt passed in from cli
+        prompt = add_cap_ref(
+            prompt,
+            f"Use this {POSTGRES_TABLE_DEFINITIONS_CAP_REF} to satisfy the database SQL query.",
+            POSTGRES_TABLE_DEFINITIONS_CAP_REF,
+            table_definitions,
+        )
 
-    prompt = llm.add_cap_ref(
-        args.prompt, 
-        f"Use these {POSTGRES_TABLE_DEFINITIONS_CAP_REF} to satisfy the database query.", 
-        POSTGRES_TABLE_DEFINITIONS_CAP_REF,
-        table_definitions
-    )
+        print("Promopt after adding table definitions:", prompt)
 
-    print("prompt v2", prompt)
+        config_list= autogen.config_list_from_json(
+           env_or_file="OAI_CONFIG_LIST",
+            filter_dict={
+                "model": ["gpt-3.5-turbo-1106"],
+            }
+)
 
-    prompt = llm.add_cap_ref(
-        prompt, 
-        f"\n\nRespond in this format {RESPONSE_FORMAT_CAP_REF}. Replace the text between <> with it's request. I need to be able to easily parse the sql query from your response.",
-        RESPONSE_FORMAT_CAP_REF,
-        f"""<explanation of the sql query>
-{SQL_DELIMITER}
-<sql query exclusively as raw text>""",
-    )
+        # build the gpt_configuration object
+        llm_config = {
+            "cache_seed": 0,
+            "temperature": 0,
+            "config_list": config_list,
+            "timeout": 120,
+            "functions": [
+                {
+                    "name": "run_sql",
+                    "description": "Run a SQL query against the postgres database",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "sql": {
+                                "type": "string",
+                                "description": "The SQL query to run against the database",
+                            }
+                        },
+                        "required": ["sql"],
+                    },
+                },
+            ],
+        }
 
-    print("\n\n ------------ PROMPT ------------")
-    print("prompt v3", prompt)
+        # build the funcation map
+        function_map = {
+            "run_sql": db.run_sql,
+        }
 
-    #call llm.prompt to get a prompt_response variable
+        # create our terminate msg function
+        def is_terminate_msg(content):
+            have_content = content.get("content", None) is not None
+            if have_content and "APPROVED"  in content["content"]:
+                return True
+            return False
+        
+        COMPLETION_PROMPT = "If everything looks good, respond with APPROVED. Otherwise, respond with REJECTED."
 
-    prompt_response = llm.prompt(prompt)
+        USER_PROXY_PROMPT = (
+            "A human admin. Interact with the Product Manager to discuss the plan. Plan execution needs to be approved by this admin."
+            + COMPLETION_PROMPT
+        )
 
-    print("\n\n ------------ PROMPT RESPONSE ------------")
-    print("prompt_response", prompt_response)
+        DATA_ENGINEER_PROMPT = (
+            "A Data Engineer. You follow a approved plan. Generate the initial SQL based on the requirements provided. Send it to the Sr Data Analyst to be executed."
+            + COMPLETION_PROMPT
+        )
 
-    # parse sql response from prompt_response using AQLQUERYDELIMITER '----------'
-    sql_query = prompt_response.split(SQL_DELIMITER)[1].strip()
+        SR_DATA_ANALYST_PROMPT = (
+            "A Sr Data Analyst. You follow an approved plan. You run the SQL query, gererate the response and send it to the product manager for final review."
+            + COMPLETION_PROMPT
+        )
 
-    print("\n\n ------------ PARSED SQL QUERY ------------")
-    print("sql_query", sql_query)
+        PRODUCT_MANAGER_PROMPT = (
+            "A Product Manager. Validate the response to make sure it's correct"
+            + COMPLETION_PROMPT
+        )
 
-    # call db_manager.run_sql() with the parsed sql
+        # create a set of agents with specific roles
+        # admin user proxy agen - takes in the prompt and manages the group chat
+        user_proxy = autogen.UserProxyAgent(
+            name="Admin",
+            system_message=USER_PROXY_PROMPT,
+            code_execution_config=False,
+            human_input_mode="NEVER",
+            is_termination_msg=is_terminate_msg,
+        )
 
-    result = db.run_sql(sql_query)
+        # data engineer agent - generates the sql query
+        data_engineer = autogen.AssistantAgent(
+            name="Engineer",
+            llm_config=llm_config,
+            system_message=DATA_ENGINEER_PROMPT,
+            code_execution_config=False,
+            human_input_mode="NEVER",
+            is_termination_msg=is_terminate_msg,
+        )
 
-    print("================== POSTGRES DATA ANALYTICS AI AGENT RESPONSE ==================")
-    print(result)
-    pass
+        # sr data analyst agent - runs the sql query and generates the response
+        sr_data_analyst = autogen.AssistantAgent(
+            name="Sr_Data_Analyst",
+            llm_config=llm_config,
+            system_message=SR_DATA_ANALYST_PROMPT,
+            human_input_mode="NEVER",
+            is_termination_msg=is_terminate_msg,
+            function_map=function_map,
+            code_execution_config={
+                "work_dir": "coding",
+                "use_docker": False,  # set to True or image name like "python:3" to use docker
+            },
+        )
+
+        # product manager agent - validates the response to make sure it's correct
+        product_manager = autogen.AssistantAgent(
+            name="Product_Manager",
+            llm_config=llm_config,
+            system_message=PRODUCT_MANAGER_PROMPT,
+            code_execution_config=False,
+            human_input_mode="NEVER",
+            is_termination_msg=is_terminate_msg,
+        )
+
+        # create a group chat with the agents and initiate the chat.
+        groupchat = autogen.GroupChat(
+            agents=[user_proxy, data_engineer, sr_data_analyst, product_manager],
+            messages=[],
+            max_round=5,
+        )
+        manager = autogen.GroupChatManager(groupchat=groupchat, llm_config=llm_config)
+        user_proxy.initiate_chat(manager, clear_history=True, message=prompt)
 
 if __name__ == '__main__':
     main()
